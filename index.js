2bc3f44615354081a5e7b3078ecee8a96b5b5955
Daniel Cueto <danielcuetorrico@gmail.com>
2023-09-05 14:49:47 -0400

    agregada la integracion con chatGPT

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 const express = require('express');
 const { MessagingResponse } = require('twilio').twiml;
 const bodyParser = require('body-parser');
+const OpenAI = require("openai");
+
 const app = express();
+
+const openai = new OpenAI({ apiKey: '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'});
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+async function getRespuesta(text) {
+  const completion = await openai.completions.create({
+    model: "text-davinci-003",
+    prompt: text,
+    max_tokens: 40,
+    temperature: 0,
+  });
 
-app.post('/sms', (req, res) => {
-    console.log(req.body);
+  return completion;
+}
+
+app.post('/sms', async (req, res) => {
+
     const twiml = new MessagingResponse();
 
     const mensaje = req.body.Body;
 
-    //consumir la api de chatGPT
-    let chatGPT;
-    if (mensaje.charAt(mensaje.length-1) === '?') {
-      const num = Math.random();
-      if (num >= 0.5) {
-        chatGPT = 'Si';
-      } else {
-        chatGPT = 'No';
-      }
-    } else {
-      chatGPT = 'Soy la respuesta de Node.js';
+    const chatGPT = await getRespuesta(mensaje);
+    let respuesta;
+    try {
+      respuesta = chatGPT.choices[0].text;
+    } catch {
+      respuesta = 'Error en chatGPT: no me puedo comunicar';
     }
-    console.log(chatGPT);
-    twiml.message(chatGPT);
+    twiml.message(respuesta);
     res.type('text/xml').send(twiml.toString());
 });
 
