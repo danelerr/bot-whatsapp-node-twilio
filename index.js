7e2afa8465e9190109e769e7a552536577af209b
Daniel Cueto <danielcuetorrico@gmail.com>
2023-09-12 19:44:16 -0400

    testeando si es que de verdad envia videos

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/sms', async (req, res) => {
 
     const mensaje = req.body.Body;
   
-    let respuesta = mensaje;
+    //let respuesta = mensaje;
     // try {
     //   const chatGPT = await getRespuesta(mensaje);
     //   respuesta = chatGPT.choices[0].text;
@@ -54,7 +54,12 @@ app.post('/sms', async (req, res) => {
     //   respuesta = 'Error en chatGPT: no me puedo comunicar';
     //   console.log(err);
     // }
-    twiml.message("Eviar un simple mensaje");
+
+    const msg = twiml.message("Tu video");
+
+    msg.media("https://d-id-talks-prod.s3.us-west-2.amazonaws.com/google-oauth2%7C117178086443174257227/tlk_OpYgwD3I9iPz6Lh7OhzQy/1694560182709.mp4?AWSAccessKeyId=AKIA5CUMPJBIK65W6FGA&Expires=1694646593&Signature=QXZ1%2Flf03mNeSP%2B7xXbm2KuSltk%3D&X-Amzn-Trace-Id=Root%3D1-6500efc1-3605f32d19c528841a3b3d50%3BParent%3D4538b8fb8efc8621%3BSampled%3D1%3BLineage%3D6b931dd4%3A0");
+
+ 
     res.type('text/xml').send(twiml.toString());
 });
 
