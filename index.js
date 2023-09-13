135303be48d1ccc5f7d4383ebe03b06cb014899c
Daniel Cueto <danielcuetorrico@gmail.com>
2023-09-12 20:26:00 -0400

    lastets verison xxx

--- index.js
+++ index.js
@@ -54,14 +54,64 @@ app.post('/sms', async (req, res) => {
     //   respuesta = 'Error en chatGPT: no me puedo comunicar';
     //   console.log(err);
     // }
+    const url = await getVideoUrl(mensaje);
 
     const msg = twiml.message("Tu video");
 
-    msg.media("https://d-id-talks-prod.s3.us-west-2.amazonaws.com/google-oauth2%7C117178086443174257227/tlk_OpYgwD3I9iPz6Lh7OhzQy/1694560182709.mp4?AWSAccessKeyId=AKIA5CUMPJBIK65W6FGA&Expires=1694646593&Signature=QXZ1%2Flf03mNeSP%2B7xXbm2KuSltk%3D&X-Amzn-Trace-Id=Root%3D1-6500efc1-3605f32d19c528841a3b3d50%3BParent%3D4538b8fb8efc8621%3BSampled%3D1%3BLineage%3D6b931dd4%3A0");
+    msg.media(url);
 
- 
     res.type('text/xml').send(twiml.toString());
 });
+
+
+
+async function getVideoUrl(text) {
+  const url = "https://api.d-id.com/talks";
+  const requestData = {
+    "script": {
+      "type": "text",
+      "input": text,
+      "provider": {
+        "type": "microsoft",
+        "voice_id": "es-BO-SofiaNeural",
+        "voice_config": {
+          "style": "Cheerful"
+        }
+      }
+    },
+    "source_url": "https://twilio-nodejs-bot-whatsapp-chatgpt.onrender.com/dariana.jpeg"
+  }
+  const answer = await fetch(url, {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": "Basic " + process.env.D_ID_API_KEY
+    },
+    body: JSON.stringify(requestData)
+  });
+  const data = await answer.json();
+  console.log(data);
+  const {id} = data;
+  console.log(id);
+  const videoRequest = await fetch(url + "/" + id, {
+    method: 'GET',
+    headers: {
+      "Accept": "*/*",
+      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": "Basic " + process.env.D_ID_API_KEY
+    }
+  });
+  const videoData = await videoRequest.json();
+  console.log(videoData);
+  const {audio_url} = videoData;
+  console.log(audio_url);
+  return audio_url;
+}
+
+
 
 
 app.listen(3000, () => {
