06e6ca1b1c0549a3552b018aad3e86920fbaa1ff
Daniel Cueto <danielcuetorrico@gmail.com>
2023-09-19 13:46:03 -0400

    cambios en el codigo, integrando y sirviedo la api de audius.org

--- index.js
+++ index.js
@@ -18,18 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//obtiene una respuesta de ChatGPT teniendo como prompt: @text
-async function getRespuesta(text) {
-  const completion = await openai.completions.create({
-    model: "text-davinci-003",
-    prompt: text,
-    max_tokens: 40,
-    temperature: 0,
-  });
-
-  return completion;
-}
-
 app.get('/', (req, res) => {
   res.send('api para topicos <strong>avanzados de progración</strong>');
 });
@@ -45,7 +33,28 @@ app.post('/sms', async (req, res) => {
     const twiml = new MessagingResponse();
 
     const mensaje = req.body.Body;
-  
+
+    const searchText = encodeURI(mensaje);
+
+    const musicSearhQuery = await fetch(`https://audius-discovery-2.theblueprint.xyz/v1/tracks/search?query=${searchText} b2b&app_name=EXAMPLEAPP`)
+      .then((res) => res.json());
+
+    const id = musicSearhQuery.data[0].id;
+
+    const audioUrl = `https://audius-discovery-2.theblueprint.xyz/v1/tracks/${id}/stream?app_name=EXAMPLEAPP`;
+    
+    const msg = twiml.message("Tu el audio solicitado: ");
+    msg.media(audioUrl);
+
+    console.log(audioUrl);
+    res.type('text/xml').send(twiml.toString());
+});
+
+
+
+//obtiene una respuesta de ChatGPT teniendo como prompt: @text
+
+    /* CODIGO PARA COMUNICARSE CON CHATGPT */
     //let respuesta = mensaje;
     // try {
     //   const chatGPT = await getRespuesta(mensaje);
@@ -54,63 +63,71 @@ app.post('/sms', async (req, res) => {
     //   respuesta = 'Error en chatGPT: no me puedo comunicar';
     //   console.log(err);
     // }
-    const url = await getVideoUrl(mensaje);
 
-    const msg = twiml.message("Tu video");
-    if (url) {
-      msg.media(url);
-    }
-
-    res.type('text/xml').send(twiml.toString());
-});
+    /* CODIGO PARA COMUNICARASE CON D-ID, NO TERMINADO AL 100% */
+    //const url = await getVideoUrl(mensaje);
 
 
+// async function getRespuesta(text) {
+//   const completion = await openai.completions.create({
+//     model: "text-davinci-003",
+//     prompt: text,
+//     max_tokens: 40,
+//     temperature: 0,
+//   });
 
-async function getVideoUrl(text) {
-  const url = "https://api.d-id.com/talks";
-  const requestData = {
-    "script": {
-      "type": "text",
-      "input": text,
-      "provider": {
-        "type": "microsoft",
-        "voice_id": "es-BO-SofiaNeural",
-        "voice_config": {
-          "style": "Cheerful"
-        }
-      }
-    },
-    "source_url": "https://twilio-nodejs-bot-whatsapp-chatgpt.onrender.com/dariana.jpeg"
-  }
-  const answer = await fetch(url, {
-    method: 'POST',
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-      "Authorization": "Basic " + process.env.D_ID_API_KEY
-    },
-    body: JSON.stringify(requestData)
-  });
-  const data = await answer.json();
-  console.log(data);
-  const {id} = data;
-  console.log(id);
-  const videoRequest = await fetch(url + "/" + id, {
-    method: 'GET',
-    headers: {
-      "Accept": "*/*",
-      "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-      "Authorization": "Basic " + process.env.D_ID_API_KEY
-    }
-  });
-  const videoData = await videoRequest.json();
-  console.log(videoData);
-  const {audio_url} = videoData;
-  console.log(audio_url);
-  return audio_url;
-}
+//   return completion;
+// }
+
+
+
+//PARA EL D-ID
+
+// async function getVideoUrl(text) {
+//   const url = "https://api.d-id.com/talks";
+//   const requestData = {
+//     "script": {
+//       "type": "text",
+//       "input": text,
+//       "provider": {
+//         "type": "microsoft",
+//         "voice_id": "es-BO-SofiaNeural",
+//         "voice_config": {
+//           "style": "Cheerful"
+//         }
+//       }
+//     },
+//     "source_url": "https://twilio-nodejs-bot-whatsapp-chatgpt.onrender.com/dariana.jpeg"
+//   }
+//   const answer = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       "Accept": "application/json",
+//       "Content-Type": "application/json",
+//       "Authorization": "Basic " + process.env.D_ID_API_KEY
+//     },
+//     body: JSON.stringify(requestData)
+//   });
+//   const data = await answer.json();
+//   console.log(data);
+//   const {id} = data;
+//   console.log(id);
+//   const videoRequest = await fetch(url + "/" + id, {
+//     method: 'GET',
+//     headers: {
+//       "Accept": "*/*",
+//       "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+//       "Accept": "application/json",
+//       "Content-Type": "application/json",
+//       "Authorization": "Basic " + process.env.D_ID_API_KEY
+//     }
+//   });
+//   const videoData = await videoRequest.json();
+//   console.log(videoData);
+//   const {audio_url} = videoData;
+//   console.log(audio_url);
+//   return audio_url;
+// }
 
 
 
