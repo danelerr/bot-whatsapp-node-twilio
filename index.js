3af191e212f6a66aa5cfcf8f4215676e156da140
Daniel Cueto <danielcuetorrico@gmail.com>
2024-01-05 18:34:26 -0400

    mesa pruea final

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new SocketServer(server, {
   cors: {
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173/guion/:id"
   }
 });
 
